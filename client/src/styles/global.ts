96a31b6816bd650ce7e386e54d8c3dd6294c032f
Matheus <matheustassodjamdjian@gmail.com>
2025-03-06 16:26:44 -0300

    add fonts

--- client/src/styles/global.ts
+++ client/src/styles/global.ts
@@ -2,22 +2,26 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle `
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap');
+
     * {
     margin: 0;
     padding: 0;
-    box-sizing: border-box;    
+    box-sizing: border-box; 
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;   
     }
 
     html {
-    font-size: 62.5%
+    font-size: 62.5%;
     }
 
     html, body, #__next {
-    height: 100%
+    height: 100%;
     }
 
     body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serfi;
+    font-family: 'Poppins', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     }
 `
 
