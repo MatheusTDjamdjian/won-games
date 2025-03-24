3cebc46a4236c5ee5d220e0ed3b30b61ff850c6f
Matheus <matheustassodjamdjian@gmail.com>
2025-03-24 17:49:36 -0300

    Creating open/close logic

--- client/src/components/Menu/test.tsx
+++ client/src/components/Menu/test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { screen } from '@testing-library/react'
+import { fireEvent, screen } from '@testing-library/react'
 
 import Menu from '.'
 import { renderWithTheme } from '@/utils/tests/helpers'
@@ -13,5 +13,28 @@ describe('<Menu />', () => {
     expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
     expect(screen.getByLabelText(/search/i)).toBeInTheDocument()
     expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument()
+  })
+
+
+  it('should render the open/close mobile menu', () => {
+    renderWithTheme(<Menu/>)
+
+    // 1. selecionar o MenuFull
+    const fullMenuElement = screen.getByRole('navigation', {hidden: true})
+
+    // 2. verificar se o menu tá escondido 
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
+
+    // 3. clicar no botão e verificar se abriu
+    fireEvent.click(screen.getByLabelText(/open menu/i)) 
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('false')
+    expect(fullMenuElement).toHaveStyle({ opacity: 1 })
+
+    // 4. clicar no botão e verificar se fechou
+    fireEvent.click(screen.getByLabelText(/close menu/i)) 
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
+
   })
 })
