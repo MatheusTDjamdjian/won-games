1f9ac69af8dd7ed344aba6f6ca9fdf5f74b10c9d
Matheus <matheustassodjamdjian@gmail.com>
2025-03-24 15:27:28 -0300

    Creating mobile menu

---
+++ client/src/components/Menu/test.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+
+import Menu from '.'
+
+describe('<Menu />', () => {
+  it('should render the heading', () => {
+    const { container } = render(<Menu />)
+
+    expect(screen.getByRole('heading', { name: /Menu/i })).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
