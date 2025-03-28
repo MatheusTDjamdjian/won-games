54e6e4059ba0b81445df251180ee8ed7aaf9a52f
Matheus <matheustassodjamdjian@gmail.com>
2025-03-28 15:48:03 -0300

    Creating styles from full menu

--- client/src/components/Menu/index.tsx
+++ client/src/components/Menu/index.tsx
@@ -7,6 +7,7 @@ import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2'
 import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outlined/ShoppingCart'
 import { Search as SearchIcon } from '@styled-icons/material-outlined/Search'
 import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
+import Button from '../Button'
 
 const Menu = () => {
   const [isOpen, setIsOpen] = useState(false)
@@ -30,6 +31,20 @@ const Menu = () => {
 
       <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
         <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
+        <S.MenuNav>
+          <S.MenuLink href="#">Home</S.MenuLink>
+          <S.MenuLink href="#">Explore</S.MenuLink>
+        </S.MenuNav>
+
+        <S.RegisterBox>
+          <Button fullWidth size="large">
+            Log in now
+          </Button>
+          <span>or</span>
+          <S.CreateAccount href="#" title="Sign Up">
+            Sign Up
+          </S.CreateAccount>
+        </S.RegisterBox>
       </S.MenuFull>
     </S.Wrapper>
 )}
