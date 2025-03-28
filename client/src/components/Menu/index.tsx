783a6e53064ff03181b257e74ee975861e930f10
Matheus <matheustassodjamdjian@gmail.com>
2025-03-28 16:28:49 -0300

    Creating rule for logged in/logged out user

--- client/src/components/Menu/index.tsx
+++ client/src/components/Menu/index.tsx
@@ -9,7 +9,11 @@ import { Search as SearchIcon } from '@styled-icons/material-outlined/Search'
 import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
 import Button from '../Button'
 
-const Menu = () => {
+export type MenuProps = {
+  username?: string
+}
+
+const Menu = ({ username } : MenuProps) => {
   const [isOpen, setIsOpen] = useState(false)
 
   return (
@@ -34,17 +38,24 @@ const Menu = () => {
         <S.MenuNav>
           <S.MenuLink href="#">Home</S.MenuLink>
           <S.MenuLink href="#">Explore</S.MenuLink>
+          {!!username && (
+            <>
+            <S.MenuLink href="#">My account</S.MenuLink>
+            <S.MenuLink href="#">Wishlist</S.MenuLink>
+          </>
+          )}
         </S.MenuNav>
-
-        <S.RegisterBox>
-          <Button fullWidth size="large">
-            Log in now
-          </Button>
-          <span>or</span>
-          <S.CreateAccount href="#" title="Sign Up">
-            Sign Up
-          </S.CreateAccount>
-        </S.RegisterBox>
+        {!username && (
+          <S.RegisterBox>
+            <Button fullWidth size="large">
+              Log in now
+            </Button>
+            <span>or</span>
+            <S.CreateAccount href="#" title="Sign Up">
+              Sign Up
+            </S.CreateAccount>
+          </S.RegisterBox>
+        )}
       </S.MenuFull>
     </S.Wrapper>
 )}
--- client/src/components/Menu/stories.tsx
+++ client/src/components/Menu/stories.tsx
@@ -1,12 +1,13 @@
+import React from 'react';
 import { Meta, StoryObj } from '@storybook/react'
-import Menu from '.'
+import Menu, { MenuProps } from '.'
 
 export default {
   title: 'Menu',
   component: Menu
 } as Meta
 
-export const Default: StoryObj = {}
+export const Default: StoryObj<MenuProps> = (args: MenuProps) => <Menu {...args} />;
 
 Default.parameters = {
   layout: "fullscreen",
