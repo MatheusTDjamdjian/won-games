81f1eb191594e93fbfec3d05d1319df7cce16545
Matheus <matheustassodjamdjian@gmail.com>
2025-03-31 15:45:11 -0300

    Creating footer

---
+++ client/src/components/Footer/stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Meta, StoryObj } from '@storybook/react'
+import Footer from '.'
+
+export default {
+  title: 'Footer',
+  component: Footer
+} as Meta
+
+export const Default: StoryObj = {
+  render: () => <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
+    <Footer/>
+  </div>
+}
