04cbf09127c3c2665fb4ce49a7ad2857a5b77afb
Matheus <matheustassodjamdjian@gmail.com>
2025-03-31 17:15:45 -0300

    create banner styles

--- client/src/components/Banner/stories.tsx
+++ client/src/components/Banner/stories.tsx
@@ -5,11 +5,14 @@ export default {
   title: 'Banner',
   component: Banner,
   args: {
-    img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+    img: '	https://ogimg.infoglobo.com.br/in/23001849-c2f-667/FT1086A/INFOCHPDPICT000041975439.jpg',
     title: 'Defy death',
     subtitle: '<p>Play the new <strong>CrashLands</strong> season',
     buttonLabel: 'Buy now',
     buttonLink: '/games/defy-death'
+  },
+  parameters: {
+    layout: 'fullscreen'
   }
 } as Meta
 
