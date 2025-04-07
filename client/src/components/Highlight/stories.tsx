1cdcef4e7f7c24abeff30d9a6c9f2a367bf0c19a
Matheus Tasso <matheustassodjamdjian@gmail.com>
2025-04-07 12:05:11 -0300

    Creating highlight component

---
+++ client/src/components/Highlight/stories.tsx
@@ -0,0 +1,15 @@
+import { Meta, StoryObj } from '@storybook/react'
+import Highlight from '.'
+
+export default {
+  title: 'Highlight',
+  component: Highlight,
+  args: {
+    title: 'Read Dead it’s back',
+    subtitle: 'Come see John’s new adventures',
+    buttonLabel: 'Buy now',
+    buttonLink: '/rdr2'
+  }
+} as Meta
+
+export const Default: StoryObj<typeof Highlight> = {}
