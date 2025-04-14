f2fd66e9973960dc35c3331b708c3bdc18b62fc7
Matheus Tasso <matheustassodjamdjian@gmail.com>
2025-04-14 14:53:03 -0300

    Creating banner slide structure and tests

---
+++ client/src/components/BannerSlider/stories.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { StoryObj, Meta } from '@storybook/react'
+import BannerSlider, { BannerSliderProps } from '.'
+ 
+ const items = [
+   {
+     img: 'https://img.freepik.com/fotos-gratis/morskie-oko-em-tatry_1204-510.jpg?t=st=1744648920~exp=1744652520~hmac=3f8a60e4f44202a56f58f8bdc45b092a016bfc9731d44feeb3202ae016c2eac9&w=996',
+     title: 'Defy death 1',
+     subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+     buttonLabel: 'Buy now',
+     buttonLink: '/games/defy-death',
+     ribbon: 'Bestselling'
+   },
+   {
+     img: 'https://img.freepik.com/fotos-gratis/bela-foto-de-colinas-gramadas-cobertas-por-arvores-perto-de-montanhas-nas-dolomitas-italia_181624-24400.jpg?t=st=1744649756~exp=1744653356~hmac=0ed752810e6d6018aa39ec467b52fd8f3a0de1386a6177e4d9d354a9aa57a5f1&w=996',
+     title: 'Defy death 2',
+     subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+     buttonLabel: 'Buy now',
+     buttonLink: '/games/defy-death'
+   },
+   {
+     img: 'https://img.freepik.com/fotos-gratis/lindo-jardim-de-morango-e-nascer-do-sol-em-doi-ang-khang-chiang-mai-tailandia_335224-762.jpg?t=st=1744649798~exp=1744653398~hmac=c8232e7038b5ff4453e7bf14b33d60e88b3bd3f91c7354052d9f2855bd523319&w=996',
+     title: 'Defy death 3',
+     subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+     buttonLabel: 'Buy now',
+     buttonLink: '/games/defy-death'
+   }
+ ]
+ 
+ export default {
+   title: 'BannerSlider',
+   component: BannerSlider,
+   args: { items },
+   parameters: {
+     layout: 'fullscreen',
+     backgrounds: {
+       default: 'dark'
+     }
+   }
+ } as Meta
+
+export const Default: StoryObj<BannerSliderProps> = {
+  render: (args) => (
+    <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
+      <BannerSlider {...args} />
+    </div>
+  )
+};
---
+++ client/src/components/BannerSlider/test.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { screen } from '@testing-library/react'
+import '../../../.jest/match-media-mock'
+
+import BannerSlider from '.'
+import { renderWithTheme } from '@/utils/tests/helpers'
+
+const items = [
+  {
+    img: 'https://img.freepik.com/fotos-gratis/morskie-oko-em-tatry_1204-510.jpg?t=st=1744648920~exp=1744652520~hmac=3f8a60e4f44202a56f58f8bdc45b092a016bfc9731d44feeb3202ae016c2eac9&w=996',
+    title: 'Defy death 1',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death',
+    ribbon: 'Bestselling'
+  },
+  {
+    img: 'https://img.freepik.com/fotos-gratis/bela-foto-de-colinas-gramadas-cobertas-por-arvores-perto-de-montanhas-nas-dolomitas-italia_181624-24400.jpg?t=st=1744649756~exp=1744653356~hmac=0ed752810e6d6018aa39ec467b52fd8f3a0de1386a6177e4d9d354a9aa57a5f1&w=996',
+    title: 'Defy death 2',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  }
+]
+
+describe('<BannerSlider />', () => {
+  it('should render vertical slider', () => {
+    const { container } = renderWithTheme(<BannerSlider items={items} />)
+
+    expect(container.querySelector('.slick-vertical')).toBeInTheDocument()
+
+  })
+
+  it('should render with 1 active item', () => {
+    const { container } = renderWithTheme(<BannerSlider items={items} />)
+
+    expect(container.querySelectorAll('.slick-slide')).toHaveLength(2)
+    expect(container.querySelectorAll('li.slick-acive')).toHaveLength(1)
+
+    expect(screen.getByRole('heading', { name: /defy death 1/i, hidden: false })).toBeInTheDocument()
+
+    expect(screen.getByRole('heading', { name: /defy death 2/i, hidden: true })).toBeInTheDocument()
+  })
+
+  it('should render with the dots', () => {
+    const { container } = renderWithTheme(<BannerSlider items={items} />)
+
+    expect(container.querySelector('.slick-dots')).toBeInTheDocument()
+  })
+})
