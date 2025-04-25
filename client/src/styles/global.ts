d12378217d25a2d3f2ce82adac683e1a295f8f21
Matheus Tasso <matheustassodjamdjian@gmail.com>
2025-04-25 14:53:18 -0300

    adjusting global styles and storybook

--- client/src/styles/global.ts
+++ client/src/styles/global.ts
@@ -1,16 +1,25 @@
 'use client'
 import { createGlobalStyle, css } from "styled-components";
 
-const GlobalStyles = createGlobalStyle `
+const GlobalStyles = createGlobalStyle <{ removeBg?: boolean }>`
+    body {
+    background: ${({ removeBg }) => (removeBg ? 'transparent' : '#fff')};
+    }
+
     * {
     margin: 0;
     padding: 0;
     box-sizing: border-box; 
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;   
+
+    &::before,
+    &::after {
+        box-sizing: inherit;
+    }
     }
 
-    ${({ theme }) => css`
+    ${({ theme, removeBg }) => css`
         html {
         font-size: 62.5%;
         }
@@ -18,6 +27,12 @@ const GlobalStyles = createGlobalStyle `
         body {
             font-family: ${theme.font.family};
             font-size: ${theme.font.sizes.medium};
+
+            ${!removeBg && 
+            css`
+                backgorund-color: ${theme.colors.mainBg};
+            `}
+            background-color: ${theme.colors.mainBg}
         }
     `}
 
