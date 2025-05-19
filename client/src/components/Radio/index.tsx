e43ea3d480d3b9588eeb0ad7f0b4e3e9a55de145
Matheus Tasso <matheustassodjamdjian@gmail.com>
2025-05-19 17:52:22 -0300

    Radio component

---
+++ client/src/components/Radio/index.tsx
@@ -0,0 +1,36 @@
+import React, { useCallback } from 'react'
+import * as S from './styles'
+import { RadioProps } from './types'
+
+
+const Radio = ({
+  label,
+  onCheck,
+  labelColor = 'white',
+  labelFor = '',
+  value,
+  ...props
+}: RadioProps) => {
+  const onChange = useCallback(() => {
+  if (onCheck) onCheck(value)
+}, [onCheck, value])
+
+  return (
+    <S.Wrapper>
+      <S.Input
+        id={labelFor}
+        type="radio"
+        value={value}
+        onChange={onChange}
+        {...props}
+      />
+      {!!label && (
+        <S.Label labelColor={labelColor} htmlFor={labelFor}>
+          {label}
+        </S.Label>
+      )}
+    </S.Wrapper>
+  )
+}
+
+export default Radio
