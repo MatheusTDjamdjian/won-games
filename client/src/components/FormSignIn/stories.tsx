31ea065af99d705198cab5cc5ba2d12833658622
Matheus Tasso <matheustassodjamdjian@gmail.com>
2025-06-02 16:27:26 -0300

    Creating the FormSignIn component

---
+++ client/src/components/FormSignIn/stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Meta, StoryObj } from '@storybook/react'
+import FormSignIn from '.'
+
+export default {
+  title: 'Form/FormSignIn',
+  component: FormSignIn
+} as Meta
+
+export const WithFloatImage: StoryObj = {
+  render: () => (
+    <div style={{ width: 300, margin: 'auto' }}>
+      <FormSignIn />
+    </div>
+  )
+};
