ef5929525dd9722ad7f441091d3bf1987359d732
Matheus Tasso <matheustassodjamdjian@gmail.com>
2025-06-02 14:08:38 -0300

    Creating a Desktop Template Style

--- client/src/templates/Auth/index.tsx
+++ client/src/templates/Auth/index.tsx
@@ -8,26 +8,34 @@ import { AuthProps } from './types'
 const Auth = ({ title, children }: AuthProps) => (
   <S.Wrapper>
     <S.BannerBlock>
-      <Logo/>
+      <S.BannerContent>
+        <Logo/>
 
-      <Heading>
-        All your favorite games in one place
-      </Heading>
-      <S.SubTitle>
-        <strong>WON</strong> is the best and most complete gaming platform.
-      </S.SubTitle>
+        <div>
+          <Heading>
+            All your favorite games in one place
+          </Heading>
+          <S.Subtitle>
+            <strong>WON</strong> is the best and most complete gaming platform.
+          </S.Subtitle>
+        </div>
 
-      <S.Footer>
-        WOn Games 2025 © Todos os Direitos Reservados
-      </S.Footer>
+        <S.Footer>
+          WOn Games 2025 © Todos os Direitos Reservados
+        </S.Footer>
+
+      </S.BannerContent>
     </S.BannerBlock>
-    <S.Content>
-      <Logo color='black' size='large'/>
-      <Heading color='black' lineColor='secondary' lineLeft>
-        {title}
-      </Heading>
 
-      {children}
+    <S.Content>
+      <S.ContentWrapper>
+        <Logo color='black' size='large'/>
+        <Heading color='black' lineColor='secondary' lineLeft>
+          {title}
+        </Heading>
+
+        {children}
+      </S.ContentWrapper>
     </S.Content>
   </S.Wrapper>
 )
