77313a69a46bd3fb54c6b01e9da5f8e54d938daa
Matheus Tasso <matheustassodjamdjian@gmail.com>
2025-06-30 16:06:39 -0300

    Add /game/[slug] page

---
+++ client/src/pages/game/[slug].tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+import { useRouter } from 'next/router'
+
+export default function Index() {
+  const router = useRouter()
+
+  console.log(router)
+
+  return <h1>{router.query.slug}</h1>
+}
