9df693b9a6f2d5ec271dad72db7ac8942ae388a2
Matheus Tasso <matheustassodjamdjian@gmail.com>
2025-07-21 12:16:44 -0300

    Add /profile/cards page

--- client/src/pages/profile/cards.tsx
+++ client/src/pages/profile/cards.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
 
-import FormProfile from "@/components/FormProfile"
+import CardsList from '@/components/CardsList'
+import { CardsListProps } from '@/components/CardsList/types'
+
+import mockCards from '../../components/PaymentOptions/mock'
 import Profile from "@/templates/Profile"
 
-export default function Me() {
+export default function ProfileCards({ cards }: CardsListProps) {
   return (
     <Profile>
-      <FormProfile />
+      <CardsList cards={cards} />
     </Profile>
   )
+}
+
+export function getServerSideProps() {
+  return {
+    props: {
+      cards: mockCards
+    }
+  }
 }
