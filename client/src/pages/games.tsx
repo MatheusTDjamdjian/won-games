a087ba69671ecb5c35d2dbef6d3b5c09e1cb9302
Matheus Tasso <matheustassodjamdjian@gmail.com>
2025-08-04 17:50:04 -0300

    Update games.tsx

--- client/src/pages/games.tsx
+++ client/src/pages/games.tsx
@@ -2,16 +2,58 @@ import React from 'react'
 import GamesTemplate, { GamesTemplateProps } from "@/templates/Games"
 
 import filterItemsMock from '@/components/ExploreSidebar/mock'
-import gamesMock from '@/components/GameCardSlider/mock'
+import { initializeApollo } from '@/utils/apollo'
+import { gql } from '@apollo/client'
 
 export default function GamesPage(props: GamesTemplateProps) {
   return <GamesTemplate {...props} />
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+
+const { data } = await apolloClient.query({
+  query: gql`
+    query QueryGames {
+      games {
+        data {
+          id
+          attributes {
+            name
+            slug
+            price
+            cover {
+              data {
+                attributes {
+                  url
+                }
+              }
+            }
+            developers {
+              data {
+                attributes {
+                  name
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `
+})
   return {
     props: {
-      games: gamesMock,
+      revalidate: 60,
+      games: data.games.data.map((game) => ({
+        title: game.attributes.name,
+        developer: game.attributes.developers.data[0]?.attributes.name ?? '',
+        img: `http://localhost:1337${game.attributes.cover?.data?.attributes?.url ?? ''}`,
+        price: new Intl.NumberFormat('en', {
+          style: 'currency',
+          currency: 'USD'
+        }).format(game.attributes.price)
+      })),
       filterItems: filterItemsMock
     }
   }
