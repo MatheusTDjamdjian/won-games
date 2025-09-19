6f92090c0221b53b45410c640d7af2959c3e16aa
Matheus Tasso <matheustassodjamdjian@gmail.com>
2025-09-19 17:11:30 -0300

    You may like these games appear on the game page

--- client/src/pages/game/[slug].tsx
+++ client/src/pages/game/[slug].tsx
@@ -5,13 +5,16 @@ import Game, { GameTemplateProps } from '@/templates/Game'
 
 import { QUERY_GAME_BY_SLUG, QUERY_GAMES } from '@/graphql/queries/games'
 import { 
+  Query_HomeQuery,
   GamesListQuery, 
   GamesListQueryVariables, 
   GameBySlugQueryQuery, 
   GameBySlugQueryQueryVariables 
 } from '@/graphql/generated'
 
-import gamesMock from '@/components/GameCardSlider/mock'
+import { gamesMapper } from '@/utils/mappers'
+import { QUERY_HOME } from '@/graphql/queries/home'
+
 
 const apolloClient = initializeApollo()
 
@@ -33,46 +36,48 @@ export async function getStaticPaths() {
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
+  // Busca o jogo pelo slug
   const { data } = await apolloClient.query<GameBySlugQueryQuery, GameBySlugQueryQueryVariables>({
     query: QUERY_GAME_BY_SLUG,
     variables: { slug: String(params?.slug) }
   });
 
+  // Busca os jogos futuros para recomendação
+  const { data: homeData } = await apolloClient.query<Query_HomeQuery>({
+    query: QUERY_HOME
+  });
+
   const gameData = data.games[0];
+  // Use os jogos futuros da home
+  const upcomingGames = homeData.upcomingGames ?? [];
 
   if (!gameData) return { notFound: true };
 
-    return {
-      props: {
-        revalidate: 60,
-
-        cover: gameData.cover?.url || null,
-
-        gameInfo: {
-          title: gameData.name,
-          price: gameData.price ?? 0,
-          description: gameData.short_description || ''
-        },
-
-        gallery: gameData.gallery
-          .filter((image) => image !== null)
-          .map((image) => ({
-            src: image.url ? `http://localhost:1337${image.url}` : '',
-            label: image.name || ''
-          })) || [],
-
-        description: gameData.description || '',
-
-        details: {
-          developer: gameData.developers?.[0]?.name || '',
-          releaseDate: gameData.release_date || '',
-          platforms: gameData.platforms?.map((p) => p?.name),
-          publisher: gameData.publisher?.name || '',
-          rating: gameData.rating || null,
-          genres: gameData.categories?.map((c) => c?.name),
-
-        recommendedGames: gamesMock
-      }
+  return {
+    props: {
+      revalidate: 60,
+      cover: gameData.cover?.url || null,
+      gameInfo: {
+        title: gameData.name,
+        price: gameData.price ?? 0,
+        description: gameData.short_description || ''
+      },
+      gallery: gameData.gallery
+        .filter((image) => image !== null)
+        .map((image) => ({
+          src: image.url ? `http://localhost:1337${image.url}` : '',
+          label: image.name || ''
+        })) || [],
+      description: gameData.description || '',
+      details: {
+        developer: gameData.developers?.[0]?.name || '',
+        releaseDate: gameData.release_date || '',
+        platforms: gameData.platforms?.map((p) => p?.name),
+        publisher: gameData.publisher?.name || '',
+        rating: gameData.rating || null,
+        genres: gameData.categories?.map((c) => c?.name),
+      },
+      recommendedGames: gamesMapper(upcomingGames),
     }
   }
 }
